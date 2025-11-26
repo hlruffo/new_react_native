cfd0b13b60bc33ba06b4bad25003b0927b38103b
Hugolr <hugo.ruffo@extreme.digital>
2025-11-26 10:32:35 -0300

    estiliza a tela de criação de tarefas

--- app/(tabs)/add-habbit.tsx
+++ app/(tabs)/add-habbit.tsx
@@ -1,9 +1,46 @@
-import { View, Text } from 'react-native';
+import { View, StyleSheet} from 'react-native';
+import { SegmentedButtons, TextInput, Button } from 'react-native-paper';
+
+
+const FREQUENCIES = ["diário", "semanal", "mensal"]
 
 export default function AddHabbitScreen() {
-  return (
-    <View>
-      <Text>This is the add habbit page.</Text>
-    </View>
-  );
+    return (
+        <View style={ styles.container }>
+            <TextInput label="Título" mode="outlined" style={ styles.input } />
+            <TextInput label="Descrição" mode="outlined" style={ styles.input }/>
+            <View style={ styles.frequencyContainer }>
+                <SegmentedButtons 
+                    buttons={FREQUENCIES.map((freq)=>({
+                            value:freq,
+                            label:freq.charAt(0).toUpperCase() + freq.slice(1),
+                        })
+                    )}
+                    style={ styles.segmentedButtons }
+                />
+            </View>
+        <Button mode="contained" style={ styles.buttons }>Adicionar tarefa</Button>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+        container:{
+            flex:1,
+            padding:16,
+            backgroundColor:"#f5f5f5",
+            justifyContent: "center"
+        },
+        input:{
+            backgroundColor:"#f5f5f5",
+            marginBottom:16,
+        },
+        frequencyContainer:{
+            marginBottom:24,
+        },
+        segmentedButtons:{
+        },
+        buttons:{
+        }
+    }
+)
